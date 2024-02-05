f6401a69f42924a3542650e723b35ff0ef4d9d7d
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-02-05 09:34:40 +0530

    Login/Signup

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -39,7 +39,7 @@ function Header() {
 
 
   return (
-    <header className='py-3 shadow bg-gray-500'>
+    <header className='py-3 shadow dark:bg-gray-800'>
       <Container>
         <nav className='flex'>
           <div className='mr-4'>
@@ -54,7 +54,7 @@ function Header() {
               <li key={item.name}>
                 <button
                 onClick={() => Navigate(item.slug)}
-                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                className='inline-bock px-6 py-2 duration-200 hover:bg-gray-400 rounded-full text-white'
                 >{item.name}</button>
               </li>
             ) : null
