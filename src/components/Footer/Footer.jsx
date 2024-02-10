2157533fb82296099d2ad66e0ff25c85ffeaace3
shubam rajput <113655062+SHUBHAMRAJPUT9665@users.noreply.github.com>
2024-02-10 17:53:21 +0530

    next

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -50,7 +50,7 @@ function Footer() {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 All Rights Reserved.
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 font-bold">© 2024 All Rights Reserved Developed By <span className='font-bold text-yellow-200 hover:cursor-pointer'>SHUBHAM RAJPUT</span>
             </span>
             <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
                 <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
